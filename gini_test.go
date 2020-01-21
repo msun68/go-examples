81017339d980bcb98c5f4a30b86a81e66c7a6dda
Michael Sun <michael@msun.net>
2020-01-21 13:54:28 -0800

    update gini example

--- gini_test.go
+++ gini_test.go
@@ -8,6 +8,11 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+var (
+	categories = map[string]int{"A": 1, "B": 2}
+	domains    = map[string]int{"A": 3, "B": 4}
+)
+
 //
 // Giving the email filtering criteria:
 //
@@ -33,6 +38,7 @@ func TestGini(t *testing.T) {
 
 		Convey(`It should be satisfiable when "email is category A" and "email from domain A"`, func() {
 			So(isSatisfiable(g, 1, 3), ShouldBeTrue)
+			So(model(g), ShouldResemble, []int{1, -2, 3, 4})
 		})
 
 		Convey(`It should not be satisfiable when "email is category A" and "email is not from domain A`, func() {
@@ -41,10 +47,12 @@ func TestGini(t *testing.T) {
 
 		Convey(`It should be satisfiable when "email is category B" and "email from domain A"`, func() {
 			So(isSatisfiable(g, 2, 3), ShouldBeTrue)
+			So(model(g), ShouldResemble, []int{-1, 2, 3, 4})
 		})
 
 		Convey(`It should be satisfiable when "email is category B" and "email is not from domain A"`, func() {
 			So(isSatisfiable(g, 2, -3), ShouldBeTrue)
+			So(model(g), ShouldResemble, []int{-1, 2, -3, 4})
 		})
 
 		Convey(`It should be satisfiable when "email is category A"`, func() {
@@ -73,6 +81,28 @@ func TestGini(t *testing.T) {
 
 			g.Untest()
 		})
+
+		Convey(`When email is from domain A, it should be classified as category A`, func() {
+			So(eval(g, "A"), ShouldEqual, "A")
+		})
+
+		Convey(`When email is from domain B, it should be classified as category B`, func() {
+			So(eval(g, "B"), ShouldEqual, "B")
+		})
+
+		Convey(`When email is from domain C (undefined), it should return empty (can not be classified)`, func() {
+			So(eval(g, "C"), ShouldEqual, "")
+		})
+
+		Convey(`It should return a solution for "email is category A"`, func() {
+			So(isSatisfiable(g, 1), ShouldBeTrue)
+			So(model(g), ShouldResemble, []int{1, -2, 3, 4})
+		})
+
+		Convey(`It should return a solution for "email is category B"`, func() {
+			So(isSatisfiable(g, 2), ShouldBeTrue)
+			So(model(g), ShouldResemble, []int{-1, 2, 3, 4})
+		})
 	})
 }
 
@@ -92,4 +122,27 @@ func assume(g *gini.Gini, assumptions ...int) {
 func isSatisfiable(g *gini.Gini, assumptions ...int) bool {
 	assume(g, assumptions...)
 	return g.Solve() == 1
+}
+
+func eval(g *gini.Gini, domain string) string {
+	if d, ok := domains[domain]; ok {
+		for category, c := range categories {
+			if isSatisfiable(g, c, d) && !isSatisfiable(g, c, -d) {
+				return category
+			}
+		}
+	}
+	return ""
+}
+
+func model(g *gini.Gini) []int {
+	var m []int
+	for i := 1; i <= g.MaxVar().Pos().Dimacs(); i++ {
+		if g.Value(z.Dimacs2Lit(i)) {
+			m = append(m, i)
+		} else {
+			m = append(m, -i)
+		}
+	}
+	return m
 }
